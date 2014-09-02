0035fabf64d47f9e37db205759e72d88ac8370e9
P. Todd Decker <ptdecker@mac.com>
2014-09-01 23:02:02 -0500

    Section 1.5

--- app.js
+++ app.js
@@ -2,15 +2,25 @@
     var app = angular.module('store', [ ]);
 
     app.controller('StoreController',function() {
-        this.product = gem;
+        this.products = gems;
     });
 
-    var gem = {
+    var gems = [
+      {
         name: 'Dodecahedron',
         price: 2.95,
-        description: 'Some gems have hidden qualities beyond'
-            + 'their luster, beyond their shine... Dodeca is'
-            + 'one of those gems.'
-    }
-
+        description: 'Some gems have hidden qualities beyond '
+            + 'their luster, beyond their shine... Dodeca is '
+            + 'one of those gems.',
+        canPurchase: true,
+	soldOut: false
+      }, {
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: 'Origin of the Pentagonal Gem is unknown, hence '
+            + 'its low value. It has a very high shine and 12 sides '
+            + 'however.',
+        canPurchase: false
+      }
+    ];
 })();
